2540b94949eb54622a6e0b84b8e0a7ad466af3c8
Maurits Seelen <mauritsseelen@gmail.com>
2019-03-22 16:48:30 +0100

    docs updated

--- eindproef/app/Http/Controllers/loginController.php
+++ eindproef/app/Http/Controllers/loginController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 class loginController extends Controller
 {
     // $results = DB::select('select * from users where id = ?', [1]);
-    
+    public function index()
+    {
+
+    }
+
+    public function login()
+    {
+        
+    }
 }
